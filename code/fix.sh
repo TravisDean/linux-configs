73d1f0166422a4ea9ccff1c9011a82e97d13e67d
Travis Dean <travis.j.dean@gmail.com>
2016-03-29 10:24:52 -0400

    remove fix

--- code/fix.sh
+++
@@ -1,4 +0,0 @@
-  929  ./configure --prefix=$pre EXIV2_LIBS=-L$HOME/travis/devout/lib/ PKG_CONFIG_PATH=/home/travis/devout/lib/pkgconfig/:$PKG_CONFIG_PATH
-  931  make clean
-  932  make
-  935  make install
